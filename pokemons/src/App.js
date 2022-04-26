3eab9bf2714283dccdbd85693522be5ae7c24b3b
ermir2001 <krystian2001@autograf.pl>
2022-04-26 13:19:40 +0200

    add_type_filter

--- pokemons/src/App.js
+++ pokemons/src/App.js
@@ -7,11 +7,11 @@ import Pokemons from "./Pokemons";
 const App = () => {
     const [pokemonSearch, setPokemonSearch] = useState("");
     const [submit, setSubmit] = useState(false);
-    const [pokemonTableID, setPoakemonTableID] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    const [pokemonTableID, setPokemonTableID] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
     const [pokemonsListID, setPokemonsListID] = useState(20);
     const [pokemonList, setPokemonList] = useState();
     const [pokeApi, setPokeApi] = useState("https://pokeApi.co/api/v2/pokemon/");
-    //const [typeTable, setTableType] = useState();
+    const [typeTable, setTypeTable] = useState();
     const [theme, setTheme] = useState("lightTable")
 
 
@@ -23,7 +23,7 @@ const App = () => {
         }
         setPokemonsListID(prev=>prev+20);
         for (let i = pokemonTableID.length + 1; i < pokemonsListID + 21; i++) {
-            setPoakemonTableID(prev => [...prev, i]);
+            setPokemonTableID(prev => [...prev, i]);
         }
     }
     const handleTheme = (e, state) => {
@@ -55,29 +55,35 @@ const App = () => {
             Promise.all(pokemonTableID.map(id => fetch(pokeApi + id)))
                 .then(results => Promise.all(results.map(r => r.json())))
                 .then(pokemons => {
-                    setPokemonList(pokemons)
+                    setPokemonList(pokemons);
                 })
 
-            // fetch('https://pokeapi.co/api/v2/type')
-            //     .then(response => response.json())
-            //     .then(data => {
-            //         setTableType(data.results.map(element => element.name));
-            //     })
-            //     .catch(error => console.log("ERROR", error))
-        } else {
-            //let type = typeTable.map((element=>(element===pokemonSearch)?true:false));
-            //let count = 0;
-            // type.forEach((element=>(element===true)?count=1:null));
-            // if(count===1){
-            //      fetch(`https://pokeapi.co/api/v2/type/${pokemonSearch}`)
-            //          .then(response => response.json())
-            //             .then(pokemons => {
-            //                 console.log(pokemons.pokemon)
-            //             console.log(pokemons.pokemon.pokemon.name)
-            //             setPokemnList(pokemons.pokemon)
-            //         }).catch(error => error);
-            // }
-            //else{
+            fetch('https://pokeapi.co/api/v2/type')
+                .then(response => response.json())
+                .then(data => {
+                    let table = data.results.map(element => [element.name,element.url]);
+                    setTypeTable(table);
+                })
+                .catch(error => console.log("ERROR", error))
+        } else if(typeTable.filter((element)=>pokemonSearch===element[0]).length!==0){
+            let typeUrl;
+            typeUrl=typeTable.filter((element)=>pokemonSearch===element[0]);
+                  fetch(typeUrl[0][1])
+                      .then(response => response.json())
+                         .then(pokemons => {
+                             let table=[];
+                             for (let i = 0; i < pokemonTableID.length; i++) {
+                                 table.push(pokemons.pokemon[i].pokemon.url);
+                             }
+                             Promise.all(table.map(url => fetch(url)))
+                                 .then(results => Promise.all(results.map(r => r.json())))
+                                 .then(pokemons => {
+                                     setPokemonList(pokemons);
+                                 })
+                     })
+                        .catch(error => error);
+            }
+            else{
             let tab = [1];
             Promise.all(tab.map(() => fetch(pokeApi)))
                 .then(results => Promise.all(results.map(r => r.json())))
